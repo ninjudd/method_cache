eb4113a44249f3e1687964327121a8aaf5d939c9
Justin Balthrop <justin@geni.com>
2009-02-14 09:01:06 -0800

    refactor to use a proxy object

--- lib/method_cache.rb
+++ lib/method_cache.rb
@@ -1,38 +1,44 @@
 #require 'memcache'
 require File.dirname(__FILE__) + '/../../memcache/lib/memcache'
 
+$:.unshift(File.dirname(__FILE__))
+require 'method_cache/proxy'
+
 module MethodCache
-  VERSION = '0.5.1'
+  VERSION = '0.6.0'
 
   def cache_method(method_name, opts = {})
+    method_name = method_name.to_sym
+    proxy = opts.kind_of?(Proxy) ? opts : Proxy.new(method_name, opts)
+    
     if self.class == Class
-      if cached_methods.empty?
+      if cached_instance_methods.empty?
         include(InvalidationMethods)
         extend(MethodAdded)
       end
       
-      method_name = method_name.to_sym
-      cached_methods[method_name] = nil
+      cached_instance_methods[method_name] = nil
       begin
-        update_opts(opts, method_name)
-        
         # Replace instance method.
-        alias_method "#{method_name}_without_caching", method_name
-        define_method method_name, &opts[:method]
+        alias_method proxy.method_name_without_caching, method_name
+        define_method method_name, proxy.method_with_caching
       rescue NameError => e
         # The method has not been defined yet. We will alias it in method_added.
         # pp e, e.backtrace
       end
-      cached_methods[method_name] = opts
+      cached_instance_methods[method_name] = proxy
 
     elsif self.class == Module
       # We will alias all methods when the module is mixed-in.
       extend(ModuleAdded) if cached_module_methods.empty?
-      cached_module_methods[method_name.to_sym] = opts
+      cached_module_methods[method_name.to_sym] = proxy
     end
   end
 
   def cache_class_method(method_name, opts = {})
+    method_name = method_name.to_sym
+    proxy = opts.kind_of?(Proxy) ? opts : Proxy.new(method_name, opts)
+
     if cached_class_methods.empty?
       extend(InvalidationMethods)
       extend(SingletonMethodAdded)
@@ -41,18 +47,16 @@ module MethodCache
     method_name = method_name.to_sym
     cached_class_methods[method_name] = nil
     begin
-      update_opts(opts, method_name)
-
       # Replace class method.
       (class << self; self; end).module_eval do
-        alias_method "#{method_name}_without_caching", method_name
-        define_method method_name, &opts[:method]
+        alias_method proxy.method_name_without_caching, method_name
+        define_method method_name, proxy.method_with_caching
       end
     rescue NameError => e
       # The method has not been defined yet. We will alias it in singleton_method_added.
       # pp e, e.backtrace
     end
-    cached_class_methods[method_name] = opts
+    cached_class_methods[method_name] = proxy
   end
 
   def self.default_cache
@@ -65,39 +69,11 @@ module MethodCache
 
 private
   
-  NULL = 'NULL'
-  def method_with_caching(method_name, opts)     
-    lambda do |*args|
-      key   = method_cache_key(method_name, *args)
-      value = opts[:cache][key]
-
-      if value.nil?
-        value = self.send("#{method_name}_without_caching", *args)
-        write_value_to_cache(key, value, opts)
-      end
-      
-      value = nil if value == NULL
-      if opts[:clone] and value
-        value.clone
-      else
-        value
-      end
-    end
-  end
-
-  def update_opts(opts, method_name)
-    opts[:method_name] = method_name
-    opts[:method] ||= method_with_caching(method_name, opts)
-
-    opts[:cache] ||= MethodCache.default_cache
-    opts[:cache] = MemCache.pool[opts[:cache]] if opts[:cache].kind_of?(Symbol)
-  end
-
-  def cached_methods(method_name = nil)
+  def cached_instance_methods(method_name = nil)
     if method_name
-      cached_methods[method_name.to_sym]
+      cached_instance_methods[method_name.to_sym]
     else
-      @cached_methods ||= {}
+      @cached_instance_methods ||= {}
     end
   end
 
@@ -118,86 +94,35 @@ private
   end
 
   module InvalidationMethods
-    def invalidate_cached_method(method_name, *args)
-      opts = cached_method_opts(method_name)
-      key  = method_cache_key(method_name, *args)
-      if block_given?
-        # Only invalidate if the block returns true.
-        value = opts[:cache][key]
-        return if value and not yield(value)
-      end
-      opts[:cache].delete(key)
+   def invalidate_cached_method(method_name, *args, &block)
+      cached_method(method_name).invalidate(self, *args, &block)
     end
 
     def method_value_cached?(method_name, *args)
-      opts = cached_method_opts(method_name)
-      key  = method_cache_key(method_name, *args)
-      not opts[:cache][key].nil?
+      cached_method(method_name).cached?(self, *args)
     end
     
-    def update_cached_method(method_name, *args)
-      opts  = cached_method_opts(method_name)
-      key   = method_cache_key(method_name, *args)
-      value = block_given? ? yield(opts[:cache][key]) : self.send("#{method_name}_without_caching", *args)
-      write_value_to_cache(key, value, opts)
-      value
+    def update_cached_method(method_name, *args, &block)
+      cached_method(method_name).update(self, *args, &block)
     end
 
   private
 
-    def write_value_to_cache(key, value, opts)
-      if opts[:cache].kind_of?(Hash)
-        raise 'expiry not permitted when cache is a Hash' if opts[:expiry]
-        opts[:cache][key] = value
-      else
-        expiry = opts[:expiry].kind_of?(Proc) ? opts[:expiry].call(value) : opts[:expiry]
-        value  = value.nil? ? NULL : value
-        opts[:cache].set(key, value, expiry)
-      end
-    end
-
-    def cached_method_opts(method_name)
+    def cached_method(method_name)
       if self.kind_of?(Class)
-        opts = cached_class_methods(method_name)
+        proxy = cached_class_methods(method_name)
       else
-        opts = self.class.send(:cached_methods, method_name)
+        proxy = self.class.send(:cached_instance_methods, method_name)
       end
-      raise "method '#{method_name}' not cached" unless opts
-      opts
-    end
-
-    def method_cache_key(*args)
-      args.unshift(self)
-
-      arg_string = args.collect do |arg|
-        case arg
-        when Class
-          class_key(arg)
-        when defined?(ActiveRecord::Base) && ActiveRecord::Base
-          "#{class_key(arg.class)}-#{arg.id}"
-        when Symbol, String, Numeric
-          arg.to_s
-        else
-          hash = arg.respond_to?(:string_hash) ? arg.string_hash : arg.hash
-          "#{class_key(arg.class)}-#{hash}"
-        end
-      end.join(',')
-      "m:#{arg_string}"
-    end
-
-    def class_key(klass)
-      key = klass.respond_to?(:version) ? "#{klass.name}_#{klass.version}" : klass.name
-      if MethodCache.partition_environments and RAILS_ENV != 'production'
-        key << "_#{RAILS_ENV}"
-      end
-      key
+      raise "method '#{method_name}' not cached" unless proxy
+      proxy
     end
   end
 
   module MethodAdded
     def method_added(method_name)
-      if opts = cached_methods(method_name)
-        cache_method(method_name, opts)
+      if proxy = cached_instance_methods(method_name)
+        cache_method(method_name, proxy)
       end
       super
     end
@@ -205,8 +130,8 @@ private
 
   module SingletonMethodAdded
     def singleton_method_added(method_name)
-      if opts = cached_class_methods(method_name)
-        cache_class_method(method_name, opts)
+      if proxy = cached_class_methods(method_name)
+        cache_class_method(method_name, proxy)
       end
       super
     end
@@ -215,15 +140,15 @@ private
   module ModuleAdded
     def extended(mod)
       mod.extend(MethodCache)
-      cached_module_methods.each do |method_name, opts|
-        mod.cache_class_method(method_name, opts)
+      cached_module_methods.each do |method_name, proxy|
+        mod.cache_class_method(method_name, proxy)
       end
     end
 
     def included(mod)
       mod.extend(MethodCache)
-      cached_module_methods.each do |method_name, opts|
-        mod.cache_method(method_name, opts)
+      cached_module_methods.each do |method_name, proxy|
+        mod.cache_method(method_name, proxy)
       end
     end
   end
---
+++ lib/method_cache/proxy.rb
@@ -0,0 +1,118 @@
+module MethodCache
+  class Proxy
+    attr_reader :method_name
+
+    def initialize(method_name, opts)
+      @method_name = method_name
+      @opts        = opts
+    end
+
+    def invalidate(*args)
+      key = cache_key(*args)
+
+      if block_given?
+        # Only invalidate if the block returns true.
+        value = cache[key]
+        return if value and not yield(value)
+      end
+      cache.delete(key)
+    end
+
+    def cached?(*args)
+      key = cache_key(*args)
+      not cache[key].nil?
+    end
+    
+    def update(*args)
+      key   = cache_key(*args)
+      value = block_given? ? yield(cache[key]) : args.first.send(:method_name_without_caching, *args)
+      write_to_cache(key, value)
+      value
+    end
+
+    def value(target, *args)
+      key   = cache_key(target, *args)
+      value = cache[key]
+          
+      if value.nil?
+        value = target.send(method_name_without_caching, *args)
+        write_to_cache(key, value)
+      end
+          
+      value = nil if value == NULL
+      if clone? and value
+        value.clone
+      else
+        value
+      end
+    end
+
+    NULL = 'NULL'
+    def method_with_caching
+      proxy = self # Need access to the proxy in the closure.
+
+      lambda do |*args|
+        proxy.value(self, *args)
+      end
+    end
+
+    def method_name_without_caching
+      "#{method_name}_without_caching"
+    end
+
+    def cache
+      if @cache.nil?
+        @cache = @opts[:cache] || MethodCache.default_cache
+        @cache = MemCache.pool[@cache] if @cache.kind_of?(Symbol)
+      end
+      @cache
+    end
+
+    def expiry
+      @opts[:expiry]
+    end
+
+    def clone?
+      !!@opts[:clone]
+    end
+
+    def write_to_cache(key, value)
+      if cache.kind_of?(Hash)
+        raise 'expiry not permitted when cache is a Hash' if expiry
+        cache[key] = value
+      else
+        expiry = expiry.kind_of?(Proc) ? expiry.call(value) : expiry
+        value  = value.nil? ? NULL : value
+        cache.set(key, value, expiry)
+      end
+    end
+
+    def cache_key(target, *args)
+      args.unshift(method_name)
+      args.unshift(target)
+
+      arg_string = args.collect do |arg|
+        case arg
+        when Class
+          class_key(arg)
+        when defined?(ActiveRecord::Base) && ActiveRecord::Base
+          "#{class_key(arg.class)}-#{arg.id}"
+        when Symbol, String, Numeric
+          arg.to_s
+        else
+          hash = arg.respond_to?(:string_hash) ? arg.string_hash : arg.hash
+          "#{class_key(arg.class)}-#{hash}"
+        end
+      end.join(',')
+      "m:#{arg_string}"
+    end
+
+    def class_key(klass)
+      key = klass.respond_to?(:version) ? "#{klass.name}_#{klass.version}" : klass.name
+      if MethodCache.partition_environments and RAILS_ENV != 'production'
+        key << "_#{RAILS_ENV}"
+      end
+      key
+    end
+  end
+end
