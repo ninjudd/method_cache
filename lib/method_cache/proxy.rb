fac8392e45a6d3f236b2bb8e3c52585436e48afe
Justin Balthrop <git@justinbalthrop.com>
2013-03-08 14:04:18 -0800

    allow custom method_cache_key

--- lib/method_cache/proxy.rb
+++ lib/method_cache/proxy.rb
@@ -207,6 +207,8 @@ module MethodCache
         '[' + arg.collect {|item| object_key(item)}.join(',') + ']'
       when defined?(ActiveRecord::Base) && ActiveRecord::Base
         "#{class_key(arg.class)}-#{arg.id}"
+      elsif arg.respond_to?(:method_cache_key)
+        arg.method_cache_key
       else
         hash = local? ? arg.hash : Marshal.dump(arg).hash
         "#{class_key(arg.class)}-#{hash}"
